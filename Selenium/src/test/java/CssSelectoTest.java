d8652806438c5f396b767c7617c3b9c560125df0
MBAtestowe <naprawdeniewiemcowymyslic@gmail.com>
2024-09-19 13:36:48 +0200

    selectorTest update

--- Selenium/src/test/java/CssSelectoTest.java
+++ Selenium/src/test/java/CssSelectoTest.java
@@ -12,5 +12,21 @@ public class CssSelectoTest {
         driver.findElement(By.cssSelector(".topSecret"));
         driver.findElement(By.cssSelector("input"));
         driver.findElement(By.cssSelector("[name='fname']"));
+        driver.findElement(By.cssSelector("*")); //all
+        driver.findElement(By.cssSelector("div ul"));
+        driver.findElement(By.cssSelector("table tr"));
+        driver.findElement(By.cssSelector("tbody tr"));
+        driver.findElement(By.cssSelector("div > ul")); //firstchild
+        driver.findElement(By.cssSelector("tbody > tr"));
+        driver.findElement(By.cssSelector("label + form")); //followingsibling
+        driver.findElement(By.cssSelector("label ~ form")); //allfollowingsiblings
+
+        driver.findElement(By.cssSelector("[name*='name']"));  //contains name
+        driver.findElement(By.cssSelector("[name^='user']"));  //starting from user
+        driver.findElement(By.cssSelector("[name$='name']"));  // ending at name
+
+        driver.findElement(By.cssSelector("li:first-child"));
+        driver.findElement(By.cssSelector("li:last-child"));
+        driver.findElement(By.cssSelector("li:nth-child(2)"));  //counting starts from 1 not 0!
     }
 }
